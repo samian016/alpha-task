6fe1c933d264ebb19a38d214193d84bfd37190df
samian016 <walidbinjashim@gmail.com>
2022-04-05 21:22:30 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 import Login from './Components/Login/Login';
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/success" element={<><h1>Successfull</h1></>} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './login.css'
 import phone from "../../image/banner.png"
 import logo from "../../image/logo.png"
@@ -6,27 +6,77 @@ import { FaFacebook } from 'react-icons/fa';
 import googl from '../../image/google.png'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const setEmailhand = (e) => {
+        setEmail(e.target.value);
+    }
+    const setPasshand = (e) => {
+        setPassword(e.target.value);
+    }
+    const onSubmitHand = (e) => {
+        e.preventDefault();
+        // console.log(email, password);
+        fetch("http://localhost:5000/login", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ email: email, password: password }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data) {
+                    alert("login successful");
+                }
+                else {
+
+                    alert("Incorrect email or password");
+                }
+            })
+    }
+    const googleLogin = () => {
+        window.open("http://localhost:5000/google")
+}
+    const facebookLogin = () => {
+        window.open("http://localhost:5000/auth/facebook")
+}
+    //pioneer@alpha.com
+    //pioneer@alpha
+    useEffect(() => {
+        /*
+          fetch("http://localhost:5000/register/demoUser", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ email: "pioneer@alpha.com", password: "pioneer@alpha" }),
+        }) 
+         */
+    }, [])
+
+
     return (
         <div className='login'>
-            
+
             <div style={{ display: 'flex', justifyContent: "space-between" }} className='login-2'>
                 <div>
-                    <img style={{ height: '85%', position: "relative",left:"50%", top: "10%" }} src={phone} alt="" /></div>
+                    <img style={{ height: '85%', position: "relative", left: "50%", top: "10%" }} src={phone} alt="" /></div>
                 <div className="formShape">
-                    <div className="marginal" style={{ height:"75%", width:"80%"}} >
+                    <div className="marginal" style={{ height: "75%", width: "80%" }} >
                         <img src={logo} alt="" />
                         <h2 style={{ marginBottom: "10%", fontSize: '30px', fontWeight: '400', fontStyle: 'normal', lineHeight: "42px", color: "#473558" }}>Explore new courses... Hurry up!!</h2>
                         <div style={{ position: 'relative' }}>
 
-                            <form action="">
+                            <form onSubmit={onSubmitHand}>
                                 <div style={{ position: 'relative' }}>
-                                    <input style={{}} className="inputField" placeholder="Email Address" type="email" name="email" id="email" /> <br />
-                                    <input placeholder="Password" className="inputField" type="password" name="email" id="email" />
+                                    <input onChange={setEmailhand} required style={{}} className="inputField" placeholder="Email Address" type="email" name="email" id="email" /> <br />
+                                    <input onChange={setPasshand} required placeholder="Password" className="inputField" type="password" name="password" id="password" />
                                     <br />
                                     <a href="/someWhere" className="forgotPass" style={{ position: "absolute", bottom: "0", right: "8%", textDecoration: "none" }} >Forgot Password?</a>
                                 </div>
                                 <div>
-                                    <button style={{ marginTop: "5%", width: "380px" }} type='submit'
+                                    <button style={{ marginTop: "5%" }} type='submit'
                                         className='buttonSubmit'
                                     >LOG IN</button>
                                 </div>
@@ -37,15 +87,15 @@ const Login = () => {
                                 </div>
                             </form>
                             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                <button onClick={() => console.log("kliik")} style={{ marginRight: "5%", borderRadius: "45%", backgroundColor: "transparent", border: "0 solid transparent" }} >
+                                <button onClick={facebookLogin} style={{ marginRight: "5%", borderRadius: "45%", backgroundColor: "transparent", border: "0 solid transparent" }} >
                                     <FaFacebook style={{ width: "55px", height: "55px", color: "#3B5998" }}></FaFacebook></button>
-                                <button style={{ borderRadius: "45%", backgroundColor: "transparent", MarginLeft: "5%", border: "0 solid transparent" }}>
+                                <button onClick={googleLogin}  style={{ borderRadius: "45%", backgroundColor: "transparent", MarginLeft: "5%", border: "0 solid transparent" }}>
                                     <img src={googl} alt="ugiu" />
                                 </button>
 
                             </div>
                             <h5 className='signup'>Don't have an account?
-                                <a className='' style={{ textDecoration: "none", fontWeight: "600", color:"#04acec" }}
+                                <a className='' style={{ textDecoration: "none", fontWeight: "600", color: "#04acec" }}
                                     href='#signup' > Sign Up</a></h5>
                         </div>
                     </div>
